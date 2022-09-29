c9e3cb74a794ff0b47348e61b1fb8c1cefe49b9a
sarthakjain7432 <sarthakjain7432@gmail.com>
2022-09-29 19:35:23 +0530

    --

--- browser/openboard clone/app.js
+++ browser/openboard clone/app.js
@@ -4,7 +4,7 @@ const socket = require("socket.io");
 const app = express();//initialized and server ready
 app.use(express.static("public"));
 
-let port = 4000;
+let port = process.env.PORT || 4000;//jha hum host kar rhe hai uske envronment se dependent hoke hume port automaticaly alote ho jaegi
 let server = app.listen(port, () => {
     console.log("listening to port" + port);
 })
